04e0bc04aeda95a801376b26941a06680878ef2b
OscarSilvaOfficial <oscarkaka222@gmail.com>
2020-11-24 18:32:48 -0300

    Criando relógio

---
+++ components/Count.vue
@@ -0,0 +1,48 @@
+<template>
+
+
+
+    <div>
+        <p class="counter">{{ hour }} <code>Horas</code> {{ minuts }} <code>Minutos</code> {{ seconds }} <code>Segundos</code></p>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            hour: 0,
+            minuts: 0,
+            seconds: 0
+        }
+    },
+
+    mounted() {
+        setInterval(()=>{
+            this.seconds++
+            if (this.seconds >= 60) {
+                this.seconds = 0
+                this.minuts++
+            }
+            if (this.minuts >= 60) {
+                    this.minuts = 0
+                    this.hour++
+            }
+            if (this.hour > 23){
+                this.hour = 0
+            }
+        }, 1000)
+    } 
+
+}
+</script>
+
+<style>
+
+p.counter {
+    font-size: 3rem;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,17 @@
 <template>
+<div>
   <div class="container">
     <div>
       <Logo />
       <h1 class="title">
-        nuxt-test
+        Estudo | Nuxt
       </h1>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <Count />
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
